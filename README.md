f350d2af72209e45ef451306292415ed701bfed6
Early Ehlinger <earlye@gmail.com>
2024-03-02 20:31:45 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Compile-Time Dimensional Analysis in Typescript
+Early Ehlinger
+
+## Introduction
+
+One of the things I really appreciate as an engineer working at
+Earnest is the opportunity to participate in the Engineering Learning
+Club. We meet fortnightly, and explore a wide variety of topics,
+including event-sourcing and other distributed system architectures,
+machine learning, functional programming, the Go programming language,
+and recently, Typescript.
+
+Although we do typically try to tie our studies back to our work, we
+also recognize that knowledge for knowledge sake is valuable and look
+into things that are wholly unrelated to our mission to make higher
+education affordable to all. Besides, it is the case that many
+innovations come about precisely because somebody makes an unexpected
+connection between seeminly unrelated domains.
+
+With that in mind, we recently looked at this blog post by Alexander
+Smirnov,
+[Compile-time functional programming in TypeScript](https://developers.mews.com/compile-time-functional-programming-in-typescript/),
+where he presents a mechanism to do compile-time addition and
+subtraction using the Typescript Generic Types. The basic idea is that, while Typescript does not allow direct addition, like this:
+
+```Typescript
+type Three = 1 + 2 // Syntax error. We can't add inside the compiler! 😡
+```
+
+It _does_ allow you to create types as concatenations of array types:
+
+```Typescript
+type A1 = [undefined];
+type A2 = [undefined, undefined];
+type A3 = [...A1, ...A2]; // A3 = A1 + A2! We CAN add inside the compiler! 😈
+```
+
+With that in mind, and the power provided by _Conditional Types_ in
+TypeScript 2.8, Smirnov proceeds to show us how to switch back and
+forth between representing numbers as, well, numbers, and representing
+them as arrays of a given length.
+
+## The old days of C++ Dimensional Analysis
+
+Upon seeing this, I was reminded of a 1994 book by Barton and Nackman
+(okay, the 1995 excerpt in C++ Journal) where they presented a library
+for performing dimensional analysis at compile time in C++ using
+templates. A quick introduction for those who may not be familiar:
+
+It is common in physics and engineering to double-check one's work by
+carrying units through the mathematical gyrations one goes through to
+arrive at a solution, and verify that the units make sense. For
+example, if you are asked to find the acceleration of an object, and
+you divide a distance by time, you will get units of
+meters/second. When you compare this against the expected type for
+acceleration, meters/second/second, you will very quickly notice that
+you have made an error somewhere, and have to go back and correct your
+work.
+
+However, in computing, it's not entirely uncommon to just represent
+physical values solely as numbers:
+
+```Typescript
+const l = 14; // meters
+const t = 2; // seconds
+
+const a = l/t; // this compiles. If we had dimensional
+  // analysis, we'd know that the resulting type is
+  // "meters/second" rather than "meters/second/second"
+```
+
+In the book above, Barton and Nackman show that you can use the C++
+templating engine to perform these checks for you:
+
+```C++
+auto l = 14 * meters; // updated a little to modern C++ syntax
+auto t = 2 * seconds;
+
+acceleration a = l / t; // error: cannot assign dimensioned<double,1,-1,0,0,0,0,0>, to dimensioned<double,1,-2,0,0,0,0,0>
+```
+
+What's more, because the C++ templates do not use any dynamic types
+(no virtual functions), an optimizer can very easily have this devolve
+directly to loading the values 14 and 2 into registers and then
+dividing (or better, just storing 7 directly into `a`.) In other
+words, there is theoretically no run-time cost to using this library.
+
+So what is necessary to make this work?
+
+Basically, you need to be able to use lists of signed integers as
+generic/template parameters, and to perform signed arithmetic on those
+parameters.
+
+The Smirnov post shows that this is possible!
+
+#
---
+++ typescript/src/index.ts
@@ -0,0 +1,166 @@
+// requires "exactOptionalPropertyTypes": true compiler option in tsconfig.json
+
+// Several of these type functions are borrowed from
+// https://developers.mews.com/compile-time-functional-programming-in-typescript/
+//
+// The underlying concept is borrowed from
+// https://se.inf.ethz.ch/~meyer/publications/OTHERS/scott_meyers/dimensions.pdf
+
+type ToNumber<A extends Array<unknown>> = A['length'];
+type ToSignedNumber<A extends Array<unknown>, S = 1|-1> = [A['length'],S];
+
+type ToArray<N extends number> = _ToArray<N, []>;
+type _ToArray<N extends number, A extends Array<unknown>> =
+  ToNumber<A> extends N
+  ? A
+  : _ToArray<N, [...A, undefined]>;
+
+type _Add<A extends Array<unknown>, B extends Array<unknown>> = [...A, ...B];
+type Add<A extends number, B extends number> =
+  ToNumber< _Add< ToArray<A>, ToArray<B> > >;
+
+type _Subtract<A extends Array<unknown>, B extends Array<unknown>> =
+  A extends [...infer A1, undefined] ? B extends [...infer B1, undefined]
+  ? _Subtract<A1, B1> : A : A;
+type Subtract<A extends number, B extends number> =
+  ToNumber< _Subtract< ToArray<A>, ToArray<B> > >;
+
+type _Greater<A extends Array<unknown>, B extends Array<unknown>> =
+  A extends [...infer A1, undefined] ? B extends [...infer B1, undefined]
+  ? _Greater<A1, B1> : true : false;
+type Greater<A extends number, B extends number> =
+  _Greater< ToArray<A>, ToArray<B> >;
+
+const compileTime = false;
+
+type IfEquals<T, U, Y=unknown, N=never> =
+  (<G>() => G extends T ? 1 : 2) extends
+  (<G>() => G extends U ? 1 : 2) ? Y : N;
+
+declare const exactType: <T, U>(
+  expected: U, // & IfEquals<T, U>,
+  draft: T & IfEquals<T, U>
+) => IfEquals<T, U>
+
+  type e3 = 3;
+type E3 = ToNumber<[1,2,3]>;
+compileTime && exactType({} as e3, {} as E3);
+
+type E4 = ToNumber<[1,2,3,4]>;
+//compileTime && exactType({} as e3, {} as E4); // will fail to compile
+
+type s3 = [3,1]
+type S3 = ToSignedNumber<[1,2,3],1>;
+compileTime && exactType({} as s3, {} as S3);
+
+// Zero always has positive sign.
+type SignedNumber<A, AS extends 1|-1 = 1> =
+  A extends 0 ? [A, 1] : [A,AS]
+
+type neg2 = SignedNumber<2,-1>; // -2
+type pos2 = SignedNumber<2,1>;
+type neg4 = SignedNumber<4,-1>; // -2
+type pos4 = SignedNumber<4,1>;
+
+compileTime && exactType({} as neg2,{} as neg2); // should compile
+compileTime && exactType({} as pos2,{} as pos2); // should compile
+// compileTime && exactType({} as neg4,{} as neg2); // shouldn't compile
+// compileTime && exactType({} as neg2,{} as pos2); // shouldn't compile
+
+type _SignedAdd<A extends number, B extends number, AS extends 1|-1, BS extends 1|-1> =
+  AS extends 1 ? (
+    BS extends 1 ? SignedNumber< Add<A,B>, 1 > : (
+      Greater<A,B> extends true ?
+        SignedNumber< Subtract<A,B>, 1> :
+        SignedNumber< Subtract<B,A>, -1 >
+    )
+  ) : ( // AS extends -1...
+    BS extends -1 ? SignedNumber< Add<A,B>, -1 > : (
+      Greater<A,B> extends true ?
+        SignedNumber< Subtract<A,B>, -1> :
+        SignedNumber< Subtract<B,A>, 1 >
+    )
+  );
+
+// +2 + +3
+compileTime && exactType({} as _SignedAdd<2,3,1,1>, {} as SignedNumber<5,1>); // should compile
+// compileTime && exactType({} as _SignedAdd<2,3,1,1>, {} as SignedNumber<4,-1>); // shouldn't compile
+
+// +0 + -5 = -5
+compileTime && exactType({} as _SignedAdd<0,5,1,-1>, {} as SignedNumber<5,-1>);
+// +5 + -2 = +3
+compileTime && exactType({} as _SignedAdd<5,2,1,-1>, {} as SignedNumber<3,1>);
+// +5 + -5 = 0
+compileTime && exactType({} as _SignedAdd<5,5,1,-1>, {} as SignedNumber<0,1>);
+compileTime && exactType({} as _SignedAdd<5,5,1,-1>, {} as SignedNumber<0,-1>);
+
+// -5 + 0 = -5
+compileTime && exactType({} as _SignedAdd<5,0,-1,1>, {} as SignedNumber<5,-1>);
+
+// -5 + -2 = -7
+compileTime && exactType({} as _SignedAdd<5,2,-1,-1>, {} as SignedNumber<7,-1>);
+
+// -5 + +2 = -3
+compileTime && exactType({} as _SignedAdd<5,2,-1,1>, {} as SignedNumber<3,-1>);
+
+// -5 + +8 = +3
+compileTime && exactType({} as _SignedAdd<5,8,-1,1>, {} as SignedNumber<3,1>);
+
+type Sign<SN extends SignedNumber<unknown,1|-1>> =
+  SN extends [unknown, infer S] ? S : never;
+
+type Magnitude<SN extends SignedNumber<number,1|-1>> =
+  SN extends [infer M, 1|-1] ? M : never;
+
+type SignedAdd<SNL extends SignedNumber<number,1|-1>, SNR extends SignedNumber<number,1|-1>> =
+  Magnitude<SNL> extends number ? Magnitude<SNR> extends number ?
+  _SignedAdd<Magnitude<SNL>, Magnitude<SNR>, Sign<SNL>, Sign<SNR>>
+  : never : never;
+
+type Negate<SN extends SignedNumber<number,1|-1>> =
+  SignedNumber<Magnitude<SN>, Sign<SN> extends 1 ? -1 : 1 >
+
+type Dimensioned<LENGTH,TIME> = {
+  value: number,
+  tag ?: [LENGTH,TIME],
+};
+
+function mul<
+  LL extends SignedNumber<number,1|-1>,
+  LT extends SignedNumber<number,1|-1>,
+  RL extends SignedNumber<number,1|-1>,
+  RT extends SignedNumber<number,1|-1>
+  >(L: Dimensioned<LL,LT>, R: Dimensioned<RL,RT>)
+: Dimensioned< SignedAdd<LL,RL>, SignedAdd<LT,RT> > {
+  return { value: L.value * R.value } as
+  Dimensioned< SignedAdd<LL,RL>, SignedAdd<LT,RT> >;
+}
+
+function div<
+  LL extends SignedNumber<number,1|-1>,
+  LT extends SignedNumber<number,1|-1>,
+  RL extends SignedNumber<number,1|-1>,
+  RT extends SignedNumber<number,1|-1>
+  >(L: Dimensioned<LL,LT>, R: Dimensioned<RL,RT>)
+: Dimensioned< SignedAdd<LL,Negate<RL>>, SignedAdd<LT,Negate<RT>> > {
+  return { value: L.value / R.value } as
+  Dimensioned< SignedAdd<LL,Negate<RL>>, SignedAdd<LT,Negate<RT>> >;
+}
+
+type Scalar       = Dimensioned<SignedNumber<0,1>,SignedNumber<0,1>>;  // m^0 * s^0 , or "no units"
+type Length       = Dimensioned<SignedNumber<1,1>,SignedNumber<0,1>>;  // m   * s^0 , or "m"
+type Area         = Dimensioned<SignedNumber<2,1>,SignedNumber<0,1>>;  // m^2 * s^0 , or "m^2"
+type Volume       = Dimensioned<SignedNumber<3,1>,SignedNumber<0,1>>;  // m^3 * s^0 , or "m^3"
+type Time         = Dimensioned<SignedNumber<0,1>,SignedNumber<1,1>>;  // m^0 * s^1 , or "s"
+type Velocity     = Dimensioned<SignedNumber<1,1>,SignedNumber<1,-1>>; // m   * s^-1, or "m/s"
+type Acceleration = Dimensioned<SignedNumber<1,1>,SignedNumber<2,-1>>; // m   * s^-2, or "m/s/s"
+
+const l:Length = {value:20};
+const t:Time = {value:10};
+const s:Time = {value:1};
+
+const v:Velocity     = div(l,t);
+const a:Acceleration = div(v,s);
+
+// const shouldFail:Volume = div(l,t); // This does fail if uncommented.
+console.log(v,a);
